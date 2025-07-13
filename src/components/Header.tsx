c4b23d8a342fd9878e5796d544faf55f90739ded
i know <ik39.10vevic@gmail.com>
2025-07-14 00:41:17 +0900

    4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import Image from "next/image";
 
 const navItems = [
   { name: "Home", href: "/" },
